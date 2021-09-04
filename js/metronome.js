be5d02e0a59a1fe5be0472b8587b64517d28d3ab
winstonthart <86156860+winstonthart@users.noreply.github.com>
2021-09-04 14:41:36 +0800

    Add files via upload

--- js/metronome.js
+++ js/metronome.js
@@ -469,5 +469,6 @@ var output = document.getElementById("tempo");
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
 output.value = this.value;
+Metronome.save();
 }
 
